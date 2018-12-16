db91aed26cf9e4761522763003726792cd5998ef
Spectre316 <37177410+Spectre316@users.noreply.github.com>
2018-12-17 00:50:53 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -19,36 +19,33 @@ sem_t mutex2;
 
 void insert_item(int to_insert, buffer *buff)
 {
-  int i = 0;
-  int j = 0;
-  if(buff->count!=0)
-  {
-    for (i = buff->count; i > 0; --i)
-      buff->tab[i] = buff->tab[i - 1];
-  }
-  buff->tab[0]=to_insert;
+  buff->tab[buff->count]=to_insert;
   ++buff->count;
 };
 
-void erase_item(buffer *buff)
+int erase_item(buffer *buff)
 {
+  int to_return=buff->tab[0];
   int i=0;
+  for(i=0; i<buff->count-1; i++)
+    buff->tab[i]=buff->tab[i+1];
   --buff->count;
+  return to_return;
 }
 
 void print1(int number, buffer *buff)
 {
   int i=0;
-  printf("\n\nKolejka %d: dodano element %d\n", number, buff->tab[0]);
+  printf("\n\nKolejka %d: dodano element %d\n", number, buff->tab[buff->count-1]);
   printf("Stan kolejki: ");
   for(i=0; i<buff->count; i++)
     printf("%d ", buff->tab[i]);
 }
 
-void print2(int number, buffer *buff)
+void print2(int number, int erased, buffer *buff)
 {
   int i=0;
-  printf("\n\nKolejka %d: usunieto element %d\n", number, buff->tab[buff->count]);
+  printf("\n\nKolejka %d: usunieto element %d\n", number, erased);
   printf("Stan kolejki: ");
   for(i=0; i<buff->count; i++)
     printf("%d ", buff->tab[i]);
@@ -64,70 +61,75 @@ void producer()
   sem_wait(&array_of_buffers[number]->empty);
   sem_wait(&array_of_buffers[number]->mutex);
   insert_item(new_element, array_of_buffers[number]);
-  sem_post(&array_of_buffers[number]->mutex);
   sem_wait(&mutex2);
   print1(number+1, array_of_buffers[number]);
   sem_post(&mutex2);
+  sem_post(&array_of_buffers[number]->mutex);
   sem_post(&array_of_buffers[number]->full);
 }
 
 void consumer1()
 {
+  int erased=0;
   sem_wait(&array_of_buffers[0]->full);
   sem_wait(&array_of_buffers[0]->mutex);
-  erase_item(array_of_buffers[0]);
-  sem_post(&array_of_buffers[0]->mutex);
+  erased=erase_item(array_of_buffers[0]);
   sem_wait(&mutex2);
-  print2(1, array_of_buffers[0]);
+  print2(1, erased, array_of_buffers[0]);
   sem_post(&mutex2);
+  sem_post(&array_of_buffers[0]->mutex);
   sem_post(&array_of_buffers[0]->empty);
 }
 
 void consumer2()
 {
+  int erased=0;
   sem_wait(&array_of_buffers[1]->full);
   sem_wait(&array_of_buffers[1]->mutex);
-  erase_item(array_of_buffers[1]);
-  sem_post(&array_of_buffers[1]->mutex);
+  erased=erase_item(array_of_buffers[1]);
   sem_wait(&mutex2);
-  print2(2, array_of_buffers[1]);
+  print2(2, erased, array_of_buffers[1]);
   sem_post(&mutex2);
+  sem_post(&array_of_buffers[1]->mutex);
   sem_post(&array_of_buffers[1]->empty);
 }
 
 void consumer3()
 {
+  int erased=0;
   sem_wait(&array_of_buffers[2]->full);
   sem_wait(&array_of_buffers[2]->mutex);
-  erase_item(array_of_buffers[2]);
-  sem_post(&array_of_buffers[2]->mutex);
+  erased=erase_item(array_of_buffers[2]);
   sem_wait(&mutex2);
-  print2(3, array_of_buffers[2]);
+  print2(3, erased, array_of_buffers[2]);
   sem_post(&mutex2);
+  sem_post(&array_of_buffers[2]->mutex);
   sem_post(&array_of_buffers[2]->empty);
 }
 
 void consumer4()
 {
+  int erased=0;
   sem_wait(&array_of_buffers[3]->full);
   sem_wait(&array_of_buffers[3]->mutex);
-  erase_item(array_of_buffers[3]);
-  sem_post(&array_of_buffers[3]->mutex);
+  erased=erase_item(array_of_buffers[3]);
   sem_wait(&mutex2);
-  print2(4, array_of_buffers[3]);
+  print2(4, erased, array_of_buffers[3]);
   sem_post(&mutex2);
+  sem_post(&array_of_buffers[3]->mutex);
   sem_post(&array_of_buffers[3]->empty);
 }
 
 void consumer5()
 {
+  int erased=0;
   sem_wait(&array_of_buffers[4]->full);
   sem_wait(&array_of_buffers[4]->mutex);
-  erase_item(array_of_buffers[4]);
-  sem_post(&array_of_buffers[4]->mutex);
   sem_wait(&mutex2);
-  print2(5, array_of_buffers[4]);
+  erased=erase_item(array_of_buffers[4]);
+  print2(5, erased, array_of_buffers[4]);
   sem_post(&mutex2);
+  sem_post(&array_of_buffers[4]->mutex);
   sem_post(&array_of_buffers[4]->empty);
 }
 
